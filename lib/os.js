nodejs/node

commit 120e5a24df76deb5019abec9744ace94f0f3746a

os: use %SystemRoot% or %windir% in os.tmpdir()
On Windows, respect the SystemRoot and windir environment variables,
don't default to c:\windows\temp.

lib/os.js
@@ -21,6 +21,7 @@
 
 var binding = process.binding('os');
 var util = require('util');
+var isWindows = process.platform === 'win32';
 
 exports.endianness = binding.getEndianness;
 exports.hostname = binding.getHostname;
@@ -42,11 +43,17 @@ exports.platform = function() {
 };
 
 exports.tmpdir = function() {
-  return process.env.TMPDIR ||
-         process.env.TMP ||
-         process.env.TEMP ||
-         (process.platform === 'win32' ? 'c:\\windows\\temp' : '/tmp');
-};
+  if (isWindows) {
+    return process.env.TEMP ||
+           process.env.TMP ||
+           (process.env.SystemRoot || process.env.windir) + "\\temp";
+  } else {
+    return process.env.TMPDIR ||
+           process.env.TMP ||
+           process.env.TEMP ||
+           '/tmp';
+  }
+}
 
 exports.tmpDir = exports.tmpdir;
 
@@ -54,4 +61,4 @@ exports.getNetworkInterfaces = util.deprecate(function() {
   return exports.networkInterfaces();
 }, 'getNetworkInterfaces is now called `os.networkInterfaces`.');
 
-exports.EOL = process.platform === 'win32' ? '\r\n' : '\n';
+exports.EOL = isWindows ? '\r\n' : '\n';

dreamhigh0525/node-convergence-archive

commit 0ce72db1665d134001d97ff97d55dd944f7bf312

lint
Fixes lint errors introduced in 120e5a24df76deb5019abec9744ace94f0f3746a

lib/os.js
@@ -46,14 +46,14 @@ exports.tmpdir = function() {
   if (isWindows) {
     return process.env.TEMP ||
            process.env.TMP ||
-           (process.env.SystemRoot || process.env.windir) + "\\temp";
+           (process.env.SystemRoot || process.env.windir) + '\\temp';
   } else {
     return process.env.TMPDIR ||
            process.env.TMP ||
            process.env.TEMP ||
            '/tmp';
   }
-}
+};
 
 exports.tmpDir = exports.tmpdir;
 